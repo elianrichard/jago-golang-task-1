f1dca57e2efde843e4e1a965ba11182529e1e195
elianrichard <elianrichards@gmail.com>
2026-01-31 23:56:04 +0700

    added product and category relationship

--- models/product.go
+++ models/product.go
@@ -1,8 +1,10 @@
 package models
 
 type Product struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Stock int    `json:"stock"`
-	Price int    `json:"price"`
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Stock       int       `json:"stock"`
+	Price       int       `json:"price"`
+	Category_ID *string   `json:"category_id,omitempty"`
+	Category    *Category `json:"category,omitempty"`
 }
--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -15,7 +15,11 @@ func NewProductRepository(db *sql.DB) *ProductRepository {
 }
 
 func (repo *ProductRepository) GetAll() ([]models.Product, error) {
-	query := "SELECT id, name, price, stock FROM products"
+	query := `SELECT
+  p.id, p.name, p.stock, p.price,
+  c.id, c.name, c.description
+  FROM products p
+  LEFT JOIN categories c ON p.category_id = c.id`
 	rows, err := repo.db.Query(query)
 	if err != nil {
 		return nil, err
@@ -24,19 +28,29 @@ func (repo *ProductRepository) GetAll() ([]models.Product, error) {
 
 	products := make([]models.Product, 0)
 	for rows.Next() {
-		var p models.Product
-		err := rows.Scan(&p.ID, &p.Name, &p.Price, &p.Stock)
+		var product models.Product
+		var categoryId, categoryName, categoryDescription sql.NullString
+		err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.Stock, &categoryId, &categoryName, &categoryDescription)
 		if err != nil {
 			return nil, err
 		}
-		products = append(products, p)
+
+		if categoryId.Valid && categoryName.Valid && categoryDescription.Valid {
+			product.Category = &models.Category{
+				ID:          categoryId.String,
+				Name:        categoryName.String,
+				Description: categoryDescription.String,
+			}
+		}
+
+		products = append(products, product)
 	}
 
 	return products, nil
 }
 
 func (repo *ProductRepository) Create(product *models.Product) error {
-	query := "INSERT INTO products (name, price, stock) VALUES ($1, $2, $3) RETURNING id"
+	query := "INSERT INTO products (name, price, stock, category_id) VALUES ($1, $2, $3, $4) RETURNING id"
 	// QueryRow executes the SQL query with the provided parameters (product.Name, product.Price, product.Stock)
 	// and expects at most one row in the result. It returns a *sql.Row which can be scanned.
 	// Scan then copies the columns from the matched row into the destination (&product.ID),
@@ -44,14 +58,30 @@ func (repo *ProductRepository) Create(product *models.Product) error {
 	// QueryRow is used instead of Query because we expect exactly one row (the inserted product's ID),
 	// and QueryRow is more efficient for single-row results as it automatically closes the underlying
 	// resources after Scan is called. The Product ID is also automatically filled in the variable
-	err := repo.db.QueryRow(query, product.Name, product.Price, product.Stock).Scan(&product.ID)
+	err := repo.db.QueryRow(query, product.Name, product.Price, product.Stock, product.Category_ID).Scan(&product.ID)
 	return err
 }
 
 func (repo *ProductRepository) GetById(id string) (*models.Product, error) {
-	query := "SELECT id, name, price, stock FROM products WHERE id = $1"
+	query := `SELECT
+  p.id, p.name, p.stock, p.price,
+  c.id, c.name, c.description
+  FROM products p
+  LEFT JOIN categories c ON p.category_id = c.id
+  WHERE p.id = $1`
+
 	var product models.Product
-	err := repo.db.QueryRow(query, id).Scan(&product.ID, &product.Name, &product.Price, &product.Stock)
+	var categoryId, categoryName, categoryDescription sql.NullString
+	err := repo.db.QueryRow(query, id).Scan(&product.ID, &product.Name, &product.Price, &product.Stock, &categoryId, &categoryName, &categoryDescription)
+
+	if categoryId.Valid && categoryName.Valid && categoryDescription.Valid {
+		product.Category = &models.Category{
+			ID:          categoryId.String,
+			Name:        categoryName.String,
+			Description: categoryDescription.String,
+		}
+	}
+
 	if err == sql.ErrNoRows {
 		return nil, errors.New("Product Not Found")
 	}
@@ -78,8 +108,8 @@ func (repo *ProductRepository) DeleteById(id string) error {
 }
 
 func (repo *ProductRepository) Update(product *models.Product) error {
-	query := "UPDATE products SET name = $1, price = $2, stock = $3 WHERE id = $4"
-	result, err := repo.db.Exec(query, product.Name, product.Price, product.Stock, product.ID)
+	query := "UPDATE products SET name = $1, price = $2, stock = $3, category_id = $4 WHERE id = $5"
+	result, err := repo.db.Exec(query, product.Name, product.Price, product.Stock, product.Category_ID, product.ID)
 	if err != nil {
 		return err
 	}
